1773a4bf2efb87a98d07be0191b4fa41e176ad73
Eugene <eugeneyong@jumpwintech.com>
2026-06-05 11:09:31 +0800

    fix: update DMM setup and push script paths

--- push.bat
+++ push.bat
@@ -1,2 +1,2 @@
 @echo off
-call "C:\Users\OoiJianHong\Eugene's Repo\PythonAutomation\Github_Auto\git-push.bat"
+call "C:\Users\EugeneYong\OneDrive - JumpWin Tech\Downloads\PythonAutomation\Github_Auto\git-push.bat"
